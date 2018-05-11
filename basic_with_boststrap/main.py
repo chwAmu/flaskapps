1ea3d39ddef93d5da33d1d9bbbd7a0fa76ca886c
soup01 <chris.chung1116@gmail.com>
2018-05-12 00:38:30 +0900

    add example with Bootstrap
    an example that with using bootstrap,
    flask library of url_for to set the css file location to 'static/main.css'

---
+++ basic_with_boststrap/main.py
@@ -0,0 +1,34 @@
+from flask import Flask,render_template,url_for
+
+app = Flask(__name__)
+
+#create a dummy data dictionary of list
+posts=[
+	{
+		'author':'hiroshi',
+		'title':'post1',
+		'content':'mypost1',
+		'data_posted':'2018-05-10'
+	}
+	,
+	{
+		'author':'amu',
+		'title':'post2',
+		'content':'mypost2',
+		'data_posted':'2018-05-11'
+	}
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html',posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html',title='About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
